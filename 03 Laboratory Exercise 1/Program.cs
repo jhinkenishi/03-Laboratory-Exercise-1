0cbe891d75518b3beefb7d0e8d50ef36edfee3a9
TomyThePillow <tomythepillowedition@gmail.com>
2020-10-22 17:14:12 +0800

    Add project files.

---
+++ 03 Laboratory Exercise 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Laboratory_Exercise_1
+{
+    class Program
+    {
+
+        static void DrawPyramid(int n)
+        {
+            for (int i = 1; i <= n; i++) 
+            { 
+                for (int j = i; j <= n; j++)
+                { 
+                    Console.Write("  "); 
+                } for (int k = 1; k <= 2 * i - 1; k++) 
+                { 
+                    Console.Write("*" + " "); 
+                } 
+                Console.WriteLine(); 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DrawPyramid(5);
+        }
+    }
+}
